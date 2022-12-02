82a6c1f26fbb43d0341a556ac43ca7fef30ce61b
ahlanlhw <ahlanlhw@gmail.com>
2022-12-02 10:56:58 +0800

    upd working except for delete - to refactor code

--- app.js
+++ app.js
@@ -21,26 +21,57 @@ const projSchema = new mongoose.Schema({
   const task = mongoose.model("task",projSchema);
 
 // Web application code here
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static("public"));
-let items = [];
 app.get("/",function(req,res){
-    res.render("list",{
-        listItem:items,
-        wipList:,
-        completedList:,
+    task.find(function(err,foundItem){
+        if (err){
+            console.log(err);
+        }else{
+            res.render("list",{
+                listItem:foundItem.filter(obj =>obj.taskStatus ==="Scoping"),
+                wipList:foundItem.filter(obj =>obj.taskStatus ==="Work in Progress"),
+                completedList:foundItem.filter(obj =>obj.taskStatus ==="Completed")
+            });
+        };
+        
+    })
+});
+
+app.post('/api/update',(req,res)=>{
+    task.updateOne({"_id":req.body._id.trim()},{"taskStatus":req.body.taskStatus.trim()},
+    (err,docs)=>{
+        if (err){
+            console.log(err);
+        }else{
+            console.log(`${docs} has been updated`);
+            app.get('/',(req,res)=>{
+                task.find(function(err,foundItem){
+                    if (err){
+                        console.log(err);
+                    }else{
+                        res.render("list",{
+                            listItem:foundItem.filter(obj =>obj.taskStatus ==="Scoping"),
+                            wipList:foundItem.filter(obj =>obj.taskStatus ==="Work in Progress"),
+                            completedList:foundItem.filter(obj =>obj.taskStatus ==="Completed")
+                        });
+                    };
+                    
+                })
+            })
+        };
     });
+    res.redirect('/');
 });
 
 app.post('/',function(req,res){
     let item = req.body.newItem;
-    // console.log(req.body.newItem)
     newTask = {
         task: item,
         // taskDesc: String,
@@ -55,9 +86,16 @@ app.post('/',function(req,res){
         };
     }
     );
+    res.redirect('/');
+})
 
-
-    items.push(item);
+app.post('/delete',function(req,res){
+    const checkedItemId = req.body.taskToDelete;
+    task.findByIdAndRemove(checkedItemId,function(err){
+        if (err){
+            console.log(err);
+        }else {console.log(`Successfuly deleted item ID: ${checkedItemId}`)};
+    })
     res.redirect('/');
 })
 
--- public/dragDrop.js
+++ public/dragDrop.js
@@ -3,10 +3,21 @@ const items = document.querySelectorAll(".item");
 const holders = document.querySelectorAll(".list");
 
 items.forEach(item => {
-  item.addEventListener('dragstart',()=>{
+  let dragged;
+  item.addEventListener('dragstart',(e)=>{
+  dragged = e.target;
+    const dataSendOver = {
+      "text":item.getElementsByTagName('p')[0].textContent,
+      "date":item.getElementsByTagName('p')[1].textContent,
+      "_id":item.getElementsByTagName('p')[2].textContent
+  }
+    e.dataTransfer.clearData();
+    e.dataTransfer.setData('text/plain',JSON.stringify(dataSendOver));
     item.classList.add('dragging');
+    // console.log(JSON.stringify(dataSendOver));
   })
 })
+
 items.forEach(item => {
   item.addEventListener('dragend',()=>{
     item.classList.remove('dragging');
@@ -24,18 +35,54 @@ holders.forEach(holder => {
       holder.insertBefore(item,aftEle);
     };
   });
+
+  holder.addEventListener('drop', (e) => {
+    e.preventDefault();
+    if (e.target.className === "list") {
+      dragged.parentNode.removeChild(dragged);
+      e.target.appendChild(dragged);
+    }
+    let dataReceive = JSON.parse(e.dataTransfer.getData('text'));
+    const taskStatus_dict = {
+      "scoping":"Scoping",
+      "wipList":"Work in Progress",
+      "completedList":"Completed"
+    };
+    updateBackend();
+    function updateBackend(){
+      const listName = taskStatus_dict[holder.id];
+      const taskText = dataReceive['text'];
+      const taskDate = dataReceive['date'];
+      const _id = dataReceive['_id']
+      fetch('/api/update',{
+        method:'POST',
+        headers:{
+        //   Accept:'application/json',
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+          "_id":_id,
+          "task":taskText,
+          "taskStatus":listName,
+          "taskDateCreated":taskDate
+        })
+      }).then(()=>{console.log("Post Method Success!")}).catch((err)=>{console.log(err)});
+    };
+  },false);
+
 })
+
 
 function getDragAfterElement(container, y) {
   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
   return draggableElements.reduce((closest, child) => {
-    const box = child.getBoundingClientRect()
-    const offset = y - box.top - box.height / 2
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
     if (offset < 0 && offset > closest.offset) {
-      return { offset: offset, element: child }
+      return { offset: offset, element: child };
     } else {
-      return closest
+      return closest;
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element
 }
